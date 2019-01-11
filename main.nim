bf4c9e95fc669bf4448489173d130d48c748bc24
Jeff Sandberg <paradox460@gmail.com>
2019-01-11 10:47:57 -0800

    Bump version for release v0.0.2

--- main.nim
+++ main.nim
@@ -1,7 +1,7 @@
 import strutils, unicode, parseopt, terminal
 from spurdify import processText
 
-const version = "0.0.1"
+const version = "0.0.2"
 
 const spurdo_face = """
     ▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄▄
